43eb81ce064eee2ef59c43d8081a14c3317b7e37
Mate Balogh <mate_balogh@mckinsey.com>
2023-12-23 09:39:10 +0100

    Implement more sofisticated error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +9,11 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'something went wrong' }],
   });
 };
